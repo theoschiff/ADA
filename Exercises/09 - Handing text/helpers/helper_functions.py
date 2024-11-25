289125e357231fb4c607d6dfffde3b71da53ffbf
Theo Schifferli <theo.schifferli@epfl.ch>
2024-11-25 11:57:38 +0100

    Added W10 Exercises

--- Exercises/09 - Handing text/helpers/helper_functions.py
+++ Exercises/09 - Handing text/helpers/helper_functions.py
@@ -1,5 +1,10 @@
 from string import punctuation
 
 EXCLUDE_CHARS = set(punctuation).union(set('’'))
+def clean_text(text):
+    for char in EXCLUDE_CHARS:
+        text = text.replace(char, " ")
+    return text.lower()
+
 def simple_tokeniser(text):
     return text.split()
